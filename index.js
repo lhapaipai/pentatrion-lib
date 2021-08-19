550cd8d0a9c08a1f83af4896fe0b27053ac52d29
Hugues Tavernier <hugues.tavernier@protonmail.com>
2021-08-19 08:03:05 +0200

    update exports

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-export * as downloadHelper from "./downloadHelper";
-export * as dateHelper from "./dateHelper";
-export * as apiHelper from "./apiHelper";
-export * as functionHelper from "./functionHelper";
-export { default as EventEmitter } from "./EventEmitter";
-export { default as emitEvent } from "./emitEvent";
 export { default as Animator } from "./Animator";
+export * as apiHelper from "./apiHelper";
+export * as dateHelper from "./dateHelper";
+export * as downloadHelper from "./downloadHelper";
+export { default as emitEvent } from "./emitEvent";
+export { default as EventEmitter } from "./EventEmitter";
+export * as functionHelper from "./functionHelper";
